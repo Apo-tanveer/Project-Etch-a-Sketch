8dc9f56bc51635e33f6e0259b593198b8713b943
apo-tanveer <mynameisapo6@gmail.com>
2026-01-19 07:08:19 +0500

    ///

--- script.js
+++ script.js
@@ -4,9 +4,24 @@ let clearButton     = document.querySelector("#clearButton");
 let pixelInputField = document.querySelector("#pixelInputField");
 let colorInputField = document.querySelector("#colorInputField");
 let warningBox      = document.querySelector("#warning");
+let eraser          = document.querySelector("#eraser");
 
-let pixelValue = 0;
-let colorValue = "";
+let pixelValue      = 0;
+let colorValue      = "";
+
+// ------------------------------------------------------------
+
+let isDrawing       = false;
+
+document.body.addEventListener("mousedown", ()=>{
+    isDrawing = true;
+})
+
+document.body.addEventListener("mouseup", ()=>{
+    isDrawing = false;
+})
+
+// ---------------------------------------------------------------------------
 
 submitButton.addEventListener("click", ()=>{
     pixelValue = pixelInputField.value;
@@ -15,7 +30,7 @@ submitButton.addEventListener("click", ()=>{
 });
 
 
-function createPixels (pixelValue){
+function createPixels (pixelValue, colorValue){
    container.innerHTML = "";
    warningBox.innerHTML = "";
 
@@ -27,14 +42,41 @@ function createPixels (pixelValue){
     }
     else{
         for(let i=0 ; i<pixelValue*pixelValue ; i++){
-            let pixel = document.createElement("div");
-            pixel.className = "pixel";
-            pixel.style.width = 400/pixelValue + "px";
-            pixel.style.height = 400/pixelValue + "px";
-            pixel.style.borderColor = "solid red";
+            let pixel             = document.createElement("div");
+            pixel.className       = "pixel";
+            pixel.style.width     = 400/pixelValue + "px";
+            pixel.style.height    = 400/pixelValue + "px";
+            pixel.style.border    = "1px solid red";
             pixel.style.boxSizing = "border-box";
 
             container.appendChild(pixel);
+
+//______________________________COLORING EACH BOX_________________________________________
+
+            pixel.addEventListener("mouseover", ()=>{
+                if(isDrawing){
+                    pixel.style.backgroundColor= colorValue;
+                }
+
+            pixel.addEventListener("mousedown", ()=>{
+                pixel.style.backgroundColor = colorValue;
+            })
+
+//_______________________________ERASER MODE______________________________________________
+
+            eraser.addEventListener("click", ()=>{
+                colorValue = "lightblue";
+            })
+// ______________________________ERASE EVERYTHING_________________________________________
+
+            clearButton.addEventListener("click", ()=>{
+                pixel.style.backgroundColor = "lightblue";
+            })
+
+            })
+            
         }  
     } 
 }
+
+
